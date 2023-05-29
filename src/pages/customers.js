a61693dd7b254da55a48785d89dd3a4877285415
Wassimkr <hw_kribaa@esi.dz>
2023-05-29 23:50:07 +0100

    Form Updates

--- src/pages/customers.js
+++ src/pages/customers.js
@@ -11,19 +11,29 @@ import {
   Col,
   Button,
   Card,
-  Collapse,
+  Steps,
   Form,
-  Tabs,
+  Collapse,
 } from "antd";
 
 const { Title } = Typography;
 const { TextArea } = Input;
+const { Step } = Steps;
 const { Panel } = Collapse;
-const { TabPane } = Tabs;
 
 const Page = () => {
   const [form] = Form.useForm();
   const [appProfile, setAppProfile] = useState({});
+  const [currentStep, setCurrentStep] = useState(0);
+
+  const stepData = [
+    { title: "Application", path: "Application" },
+    { title: "Application Consumers", path: "Application Consumers" },
+    { title: "Microservices", path: "Microservices" },
+    { title: "Data Sources", path: "Data Sources" },
+    { title: "Traffic And Load", path: "Traffic And Load" },
+    { title: "Deployment Context", path: "Deployment Context" },
+  ];
 
   useEffect(() => {
     fetchData();
@@ -79,12 +89,7 @@ const Page = () => {
   const handleRemove = (path, index) => {
     setAppProfile((prevState) => {
       const updatedProfile = _.cloneDeep(prevState);
-      const arrayOrObjectToModify = _.get(updatedProfile, path);
-      if (Array.isArray(arrayOrObjectToModify)) {
-        _.pullAt(arrayOrObjectToModify, index);
-      } else if (typeof arrayOrObjectToModify === "object") {
-        delete arrayOrObjectToModify[index];
-      }
+      _.pullAt(_.get(updatedProfile, path), index);
       return updatedProfile;
     });
   };
@@ -92,184 +97,171 @@ const Page = () => {
   const handleAddArrayElement = (path) => {
     setAppProfile((prevState) => {
       const updatedProfile = _.cloneDeep(prevState);
-      const arrayToModify = _.get(updatedProfile, path, []);
-      if (Array.isArray(arrayToModify)) {
-        let newElement = {};
-        switch (path) {
-          case "Application Consumers":
-            newElement = {
-              "Profile Type": "",
-              Characteristics: "",
-              "Interfaces Experiences": [
-                {
-                  "Interface Type": "",
-                  Endpoint: "",
-                },
-              ],
-              "Consumer Specific Functionality": "",
-              "Authentication And Authorization": [
-                {
-                  Method: "",
-                  Permissions: "",
-                },
-              ],
-            };
-            break;
-          case "Microservices":
-            newElement = {
-              Name: "",
-              Purpose: "",
-              Functionality: "",
-              "API Endpoints": [
-                {
-                  Endpoint: "",
-                  Purpose: "",
-                  Method: "",
-                },
-              ],
-              Dependencies: [
-                {
-                  ServiceName: "",
-                  Purpose: "",
-                },
-              ],
-              "Resource Requirements": {
-                CPU: "",
-                Memory: "",
-                Storage: "",
+      const arrayToModify = _.get(updatedProfile, path);
+      let newElement = {};
+      switch (path) {
+        case "Application Consumers":
+          newElement = {
+            "Profile Type": "",
+            Characteristics: "",
+            "Interfaces Experiences": [
+              {
+                "Interface Type": "",
+                Endpoint: "",
               },
-              "Operational Aspects": {
-                Monitoring: "",
-                Logging: "",
-                "Deployment Scaling Instructions": "",
+            ],
+            "Consumer Specific Functionality": "",
+            "Authentication And Authorization": {
+              Method: "",
+              Permissions: "",
+            },
+          };
+          break;
+        case "Microservices":
+          newElement = {
+            Name: "",
+            Purpose: "",
+            Functionality: "",
+            "API Endpoints": [
+              {
+                Endpoint: "",
+                Purpose: "",
+                Method: "",
               },
-            };
-            break;
-          case "Data Sources":
-            newElement = {
-              "Source Name": "",
-              Details: "",
-              "Data Models": [
-                {
-                  "Model Name": "",
-                  Attributes: "",
-                },
-              ],
-              "Data Access And Security": {
-                "Access Method": "",
-                "Security Measures": "",
+            ],
+            Dependencies: [
+              {
+                ServiceName: "",
+                Purpose: "",
               },
-              "Data Governance": {
-                "Management Strategy": "",
-                "Cleaning Strategy": "",
-                "Consistency Strategy": "",
+            ],
+            "Resource Requirements": {
+              CPU: "",
+              Memory: "",
+              Storage: "",
+            },
+            "Operational Aspects": {
+              Monitoring: "",
+              Logging: "",
+              "Deployment Scaling Instructions": "",
+            },
+          };
+          break;
+        case "Data Sources":
+          newElement = {
+            "Source Name": "",
+            Details: "",
+            "Data Models": [
+              {
+                "Model Name": "",
+                Attributes: "",
               },
-            };
-            break;
-          case "Traffic And Load":
-            newElement = {
-              "Traffic Type": {
-                "Request Type": "",
-                Frequency: "",
-                "Peak Times": "",
-                "Performance Requirements": "",
-              },
-              "Load Balancing Strategy": "",
-            };
-            break;
-          default:
-            newElement = {
-              "Interface Type": "",
-              Endpoint: "",
-            };
-            break;
-        }
-        arrayToModify.push(newElement);
-        _.set(updatedProfile, path, arrayToModify);
+            ],
+            "Data Access And Security": {
+              "Access Method": "",
+              "Security Measures": "",
+            },
+            "Data Governance": {
+              "Management Strategy": "",
+              "Cleaning Strategy": "",
+              "Consistency Strategy": "",
+            },
+          };
+          break;
+        case "Traffic And Load":
+          newElement = {
+            "Traffic Type": {
+              "Request Type": "",
+              Frequency: "",
+              "Peak Times": "",
+              "Performance Requirements": "",
+            },
+            "Load Balancing Strategy": "",
+          };
+          break;
+        case "Deployment Context":
+          newElement = {
+            Location: "",
+            "Cloud Platforms": "",
+            "Containerization Details": "",
+            CI_CD_Pipeline: "",
+          };
+          break;
+        default:
+          newElement = {
+            "Interface Type": "",
+            Endpoint: "",
+          };
+          break;
       }
+      arrayToModify.push(newElement);
       return updatedProfile;
     });
   };
 
   const renderForm = (data, path = "") => {
-    return Object.entries(data).map(([key, value]) => {
-      const newPath = path ? `${path}.${key}` : key;
-
-      const addRemoveButtonKeys = [
-        "Application Consumers",
-        "Microservices",
-        "Data Sources",
-        "Traffic And Load",
-      ];
-
-      const isArrayParent = addRemoveButtonKeys.includes(key);
-
-      if (Array.isArray(value)) {
-        return (
-          <Collapse key={newPath} bordered={false} defaultActiveKey={[]}>
-            <Panel header={key} key={newPath}>
-              <Collapse bordered={false} defaultActiveKey={[]}>
-                {value.map((item, index) => (
-                  <Panel header={`${key} ${index + 1}`} key={`${newPath}.${index}`}>
-                    {renderForm(item, `${newPath}[${index}]`)}
-                    <Button
-                      type="primary"
-                      danger
-                      onClick={() => handleRemove(newPath, index)}
-                      style={{ marginTop: "20px" }}
-                    >
-                      Remove {key} {index + 1}
-                    </Button>
-                  </Panel>
-                ))}
-              </Collapse>
-              {isArrayParent && (
-                <Button
-                  type="primary"
-                  onClick={() => handleAddArrayElement(newPath.split(".").slice(0, -1).join("."))}
-                  style={{ marginTop: "20px" }}
-                >
-                  Add New {key}
-                </Button>
-              )}
-            </Panel>
-          </Collapse>
-        );
-      }
-
-      if (typeof value === "object" && !Array.isArray(value)) {
-        return (
-          <Collapse key={newPath} bordered={false} defaultActiveKey={[]}>
-            <Panel header={key} key={newPath}>
-              {renderForm(value, newPath)}
-              {isArrayParent && (
-                <Button
-                  type="primary"
-                  danger
-                  onClick={() => handleRemove(path, outerIndex)}
-                  style={{ marginTop: "20px" }}
-                >
-                  Remove {key}
-                </Button>
-              )}
-            </Panel>
-          </Collapse>
-        );
-      }
-
+    if (Array.isArray(data)) {
       return (
-        <Row key={newPath} gutter={[16, 16]}>
-          <Col xs={24} sm={12}>
-            <Title level={5}>{key}</Title>
-          </Col>
-          <Col xs={24} sm={12}>
-            <Form.Item>
-              <TextArea rows={4} onChange={(e) => handleInputChange(e, newPath)} />
-            </Form.Item>
-          </Col>
-        </Row>
+        <div>
+          {data.map((item, index) => (
+            <Card title={`${path.split(".").pop()} ${index + 1}`} key={`${path}-${index}`}>
+              {renderForm(item, `${path}[${index}]`)}
+              <Button
+                type="primary"
+                danger
+                onClick={() => handleRemove(path, index)}
+                style={{ marginTop: "20px" }}
+              >
+                Remove {path.split(".").pop()} {index + 1}
+              </Button>
+            </Card>
+          ))}
+          <Button
+            type="primary"
+            onClick={() => handleAddArrayElement(path)}
+            style={{ marginTop: "20px" }}
+          >
+            Add New {path.split(".").pop()}
+          </Button>
+        </div>
       );
-    });
+    }
+
+    if (typeof data === "object" && !Array.isArray(data)) {
+      return Object.entries(data).map(([key, value]) => {
+        const newPath = path ? `${path}.${key}` : key;
+        if (typeof value === "object" || Array.isArray(value)) {
+          return (
+            <Collapse key={newPath} bordered={false} defaultActiveKey={[]}>
+              <Panel header={key} key={newPath}>
+                {renderForm(value, newPath)}
+              </Panel>
+            </Collapse>
+          );
+        } else {
+          return renderForm(value, newPath);
+        }
+      });
+    }
+
+    return (
+      <Row key={path} gutter={[16, 16]} align="top">
+        <Col xs={24}>
+          <Title level={5}>{path.split(".").pop()}</Title>
+          <Form.Item>
+            <Input placeholder="Enter value" onChange={(e) => handleInputChange(e, path)} />
+          </Form.Item>
+        </Col>
+      </Row>
+    );
+  };
+
+  const next = () => {
+    setCurrentStep(currentStep + 1);
+  };
+
+  const prev = () => {
+    setCurrentStep(currentStep - 1);
   };
 
   return (
@@ -280,45 +272,31 @@ const Page = () => {
       <div style={{ padding: "20px" }}>
         <Title level={2}>Application Profile</Title>
         <Form form={form} onFinish={handleSubmit}>
-          <Tabs type="card">
-            <TabPane tab="Application" key="1">
-              {renderForm({ Application: appProfile["Application"] || {} })}
-            </TabPane>
-            <TabPane tab="Application Consumers" key="2">
-              {renderForm(
-                { "Application Consumers": appProfile["Application Consumers"] || [] },
-                "Application Consumers",
-                true
-              )}
-            </TabPane>
-            <TabPane tab="Microservices" key="3">
-              {renderForm(
-                { Microservices: appProfile["Microservices"] || [] },
-                "Microservices",
-                true
-              )}
-            </TabPane>
-            <TabPane tab="Data Sources" key="4">
-              {renderForm(
-                { "Data Sources": appProfile["Data Sources"] || [] },
-                "Data Sources",
-                true
-              )}
-            </TabPane>
-            <TabPane tab="Traffic And Load" key="5">
-              {renderForm(
-                { "Traffic And Load": appProfile["Traffic And Load"] || [] },
-                "Traffic And Load",
-                true
-              )}
-            </TabPane>
-            <TabPane tab="Deployment Context" key="6">
-              {renderForm({ "Deployment Context": appProfile["Deployment Context"] || {} })}
-            </TabPane>
-          </Tabs>
-          <Button type="primary" htmlType="submit" style={{ marginTop: "20px" }}>
-            Submit
-          </Button>
+          <Steps current={currentStep} style={{ margin: "20px 0" }}>
+            {stepData.map((item) => (
+              <Step key={item.title} title={item.title} />
+            ))}
+          </Steps>
+          <div className="steps-content">
+            {renderForm(_.get(appProfile, stepData[currentStep].path), stepData[currentStep].path)}
+          </div>
+          <div className="steps-action">
+            {currentStep > 0 && (
+              <Button style={{ margin: "10px 8px 10px 0" }} onClick={prev}>
+                Previous
+              </Button>
+            )}
+            {currentStep < stepData.length - 1 && (
+              <Button type="primary" onClick={next} style={{ margin: "10px 0" }}>
+                Next
+              </Button>
+            )}
+            {currentStep === stepData.length - 1 && (
+              <Button type="primary" htmlType="submit" style={{ margin: "10px 0" }}>
+                Submit
+              </Button>
+            )}
+          </div>
         </Form>
       </div>
     </>
